3cf30da5a6de6a8845081d35ad7f78870b986e6e
kanya <qingxingmeixiang@163.com>
2020-06-20 15:45:01 +0800

    first commit

---
+++ src/main/java/com/ryze/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.ryze.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping
+    @ResponseBody
+    public String hello(){
+        return "hello";
+    }
+}
